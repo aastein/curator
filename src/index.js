e8fde4c509d94898400ea85bb1e5fe572608f26a
Aaron Stein <aaron@narvar.com>
2017-04-02 10:42:04 -0700

    no message

--- src/index.js
+++ src/index.js
@@ -35,16 +35,15 @@ function setCredentials(){
   return new Promise((resolve, reject) => {
     MongoClient.connect(dbUrl, (err, db) => {
       assert.equal(null, err);
-//      console.log("Connected successfully to server");
+      console.log("Connected successfully to db");
+      console.log("Setting credentials");
       var col = db.collection('credentials');
       var data = col.find({}).toArray((err, data) => {
-//        console.log("Setting credentials");
-//        console.log(data[0]);
         username = data[0].username;
         password = data[0].password;
-//        console.log("Set credentials");
+        console.log("Set credentials");
         db.close();
-//        console.log("Disconnected from server");
+        console.log("Disconnected from db");
         resolve();
       });
     });
@@ -55,26 +54,27 @@ function setStoragePath(){
   storagePath = config.filter((record) => {
     return record.storage != null;
   });
+  console.log("Set storage path to:", storagePath);
 }
 
 function setThreadName(){
   threadName = config.find((record) => {
     return record.threadName != null;
   }).threadName.replace(/\s/g, " ");
+  console.log("Set threadName to:", threadName);
 }
 
 function setConfig(){
   return new Promise((resolve, reject) => {
     MongoClient.connect(dbUrl, (err, db) => {
       assert.equal(null, err);
-//      console.log("Connected successfully to server");
+      console.log("Connected successfully to db");
+      console.log("Setting config");
       var col = db.collection('config');
       var data = col.find({}).toArray((err, data) => {
-//        console.log("Setting config");
         config = data;
-//        console.log("Set config: ", config);
         db.close();
-//        console.log("Disconnected from server");
+        console.log("Disconnected from db");
         resolve();
       });
     });
@@ -94,12 +94,12 @@ function setup(){
 
 function setSession(){
   return new Promise((resolve, reject) => {
-//    console.log("Setting session");
+    console.log("Setting session");
     device = new Client.Device(username);
     storage = new Client.CookieFileStorage(storagePath + username + '.json');
     Client.Session.create(device, storage, username, password).then((s) => {
      		session=s;
-//        console.log("Session set");
+        console.log("Set session");
         resolve(true);
   	});
   });
@@ -107,35 +107,36 @@ function setSession(){
 
 function setInboxFeed(){
   return new Promise((resolve, reject) => {
-  //  console.log("Getting inbox feed")
+    console.log("Setting inbox feed")
     new Client.Feed.Inbox(session, 100).get().then((feed) => {
-  //    console.log("Setting inbox feed", feed);
       inboxFeed = feed;
-  //    console.log("Set inbox feed");
+      console.log("Set inbox feed");
       resolve();
     });
   });
 }
 
 function setThreadWithName(){
-  //console.log("Finding thread with name:", threadName);
+  console.log("Setting thread by name:", threadName);
   thread = inboxFeed.find((thread) => {
       return thread._params.title === threadName;
   });
+  console.log("Set thread");
 }
 
 function setThreadItemFeed(){
   return new Promise((resolve, reject) => {
+    console.log("Setting thread items feed from feed");
     new Client.Feed.ThreadItems(session, thread.id, 100).get().then((feed) => {
-      //console.log("Setting thread feed", feed);
       threadFeed = feed;
-      //console.log("Set thread items feed");
+      console.log("Set thread items feed");
       resolve();
     });
   });
 }
 
 function getNewImageUrls(){
+  console.log("Setting new image urls");
   newImageUrls = threadFeed.filter((item) => {
     return item.getParams().mediaShare != null;
   }).filter((item) => {
@@ -147,18 +148,22 @@ function getNewImageUrls(){
   }).map((item) => {
     return item.getParams().mediaShare.images[0].url;
   });
+  console.log("Set new image urls:", newImageUrls);
 }
 
 function getPostedImageUrls(){
   return new Promise((resolve, reject) => {
     MongoClient.connect(dbUrl, (err, db) => {
       assert.equal(null, err);
+      console.log("Connected successfully to db");
+      console.log("Getting posted image urls");
       var col = db.collection('images');
       var data = col.find({}).toArray((err, data) => {
         postedImageUrls = data.map((d) => {
           return d.url;
         });
         db.close();
+        console.log("Disconnected from db");
         resolve();
       });
     });
@@ -167,17 +172,20 @@ function getPostedImageUrls(){
 
 function getUnpostedImageUrls(){
   unpostedImageUrls = newImageUrls.filter((url) => {
-    console.log("already posted:", postedImageUrls.includes(url))
+    console.log("Already posted:", postedImageUrls.includes(url))
     return !postedImageUrls.includes(url);
   })
+  console.log("Set unposteed image urls:", unpostedImageUrls);
 }
 
 function downloadImageByUrl(url){
   return new Promise((resolve, reject) => {
+    console.log("Downloading image by url:", url);
     var filename = 'temp/' + new Date().getTime() + '.jpg';
     filePaths.push(filename);
     request.head(url, function(err, res, body){
       request(url).pipe(fs.createWriteStream(filename)).on('close', () => {
+        console.log("Downloaded image");
         resolve();
       });
     });
@@ -186,11 +194,13 @@ function downloadImageByUrl(url){
 
 function downloadImagesByUrl(){
   return new Promise((resolve, reject) => {
+    console.log("Downloading images");
     var promiseImages = unpostedImageUrls.map((url) => {
         console.log("url:", url);
         return downloadImageByUrl(url);
     });
     Promise.all(promiseImages).then(() => {
+      console.log("Downloaded images");
       resolve();
     });
   });
@@ -198,11 +208,14 @@ function downloadImagesByUrl(){
 
 function postImages(){
   return new Promise((resolve, reject) => {
+    console.log("Posting images");
     filePaths.forEach((path) => {
       new Client.Upload.photo(session, path).then((upload) => {
+        console.log("Posting image:", path);
     		return new Client.Media.configurePhoto(session, upload.params.uploadId, '');
     	})
     	.then((medium) => {
+        console.log("Posted image");
         resolve();
     	})
     });
@@ -213,10 +226,13 @@ function recordPostedUrl(url){
   return new Promise((resolve, reject) => {
     MongoClient.connect(dbUrl, (err, db) => {
       assert.equal(null, err);
+      console.log("Connected successfully to db");
+      console.log("Recording posted image url");
       var col = db.collection('images');
       col.insertOne({"url":url}, (err, r) => {
         assert.equal(null, err);
         assert.equal(1, r.insertedCount);
+        console.log("Disconnected from db");
         db.close();
         resolve();
       });
@@ -226,10 +242,12 @@ function recordPostedUrl(url){
 
 function recordPostedUrls(){
   return new Promise((resolve, reject) => {
+    console.log("Recording posted image urls");
     var promiseInserts = unpostedImageUrls.map((url) => {
         return recordPostedUrl(url);
     });
     Promise.all(promiseInserts).then(() => {
+      console.log("Recorded posted image urls");
       resolve();
     });
   });
@@ -246,9 +264,8 @@ function main(){
             getUnpostedImageUrls();
             downloadImagesByUrl().then(() => {
               postImages().then(() => {
-                console.log("posted images");
                 recordPostedUrls().then(() => {
-                  console.log("recorded posted urls");
+                  console.log("Done");
                 });
               });
             })
